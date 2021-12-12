d93dfbc8372c7f9c51c6f7720f9e4c00110e2480
IvanCuri <Ivan.Ap2020@outlook.com>
2021-12-11 21:14:08 -0600

    inicio de header

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Olympo Web</title>
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    <h1>Hola Mundo desde Ivan</h1>
-    <?php echo "Hola mundo desde Ivan x2"; ?>
+<?php require 'includes/templates/header.php';?>
+
 </body>
 </html>
