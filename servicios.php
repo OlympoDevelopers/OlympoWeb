98e0f95ed53c2d549025d8f45dc9bb97744aeb72
anghelr14 <72480694+anghelr14@users.noreply.github.com>
2021-12-15 10:12:02 -0600

    Primer propuesta boton

--- servicios.php
+++ servicios.php
@@ -3,21 +3,34 @@
 <div class="container">
     <div class="card">
         <img src="/build/img/webDesign.webp" alt="Diseño web">
-        <h4>Creación de desarrollo web</h4>
+        <h4>Desarrollo web</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa cupiditate deleniti doloribus eius error eum fugiat, fugit incidunt, laborum necessitatibus officia quae quaerat quisquam quo, repellat saepe suscipit vel!</p>
+
+        <div class="btn">
+            <span>Contratar</span>
+        </div>
+
+
+<!--        <a href="#">Contratar</a>-->
+    </div>
+
+    <div class="card">
+        <img src="/build/img/pexels-alex-fu-1767783.webp" alt="Diseño web">
+        <h4>Edición de Video</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa cupiditate deleniti doloribus eius error eum fugiat, fugit incidunt, laborum necessitatibus officia quae quaerat quisquam quo, repellat saepe suscipit vel!</p>
         <a href="#">Contratar</a>
     </div>
 
     <div class="card">
-        <img src="/build/img/webDesign.webp" alt="Diseño web">
-        <h4>Creación de desarrollo web</h4>
+        <img src="/build/img/voxel.webp" alt="Diseño web">
+        <h4>Diseño Voxel Arts</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa cupiditate deleniti doloribus eius error eum fugiat, fugit incidunt, laborum necessitatibus officia quae quaerat quisquam quo, repellat saepe suscipit vel!</p>
         <a href="#">Contratar</a>
     </div>
 
     <div class="card">
-        <img src="/build/img/webDesign.webp" alt="Diseño web">
-        <h4>Creación de desarrollo web</h4>
+        <img src="/build/img/monitor-g1e5f3580d_1920.webp" alt="Diseño web">
+        <h4>Animación</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa cupiditate deleniti doloribus eius error eum fugiat, fugit incidunt, laborum necessitatibus officia quae quaerat quisquam quo, repellat saepe suscipit vel!</p>
         <a href="#">Contratar</a>
     </div>
