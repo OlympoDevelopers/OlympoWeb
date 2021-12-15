4902fe73efa216b14facf9633e6b1bbbf70f7b94
anghelr14 <72480694+anghelr14@users.noreply.github.com>
2021-12-15 15:05:55 -0600

    Primer diseño footer

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,4 +1,21 @@
-<footer>
+<footer class="contenido-footer">
+
+    <img src="/build/img/FavIcon.svg" alt="FavIcon Footer">
+
+    <h2>© 2021 Olympo Projects Todos los derechos reservados </h2>
+
+    <div class="icons">
+        <a href="#">
+            <i class="fab fa-whatsapp"></i>
+        </a>
+        <a href="#">
+            <i class="fab fa-github"></i>
+        </a>
+        <a href="#">
+            <i class="fas fa-envelope-open-text"></i>
+        </a>
+    </div>
+
 </footer>
 </body>
 </html>
---
+++ paginarpuebas.php
@@ -0,0 +1,7 @@
+<?php require 'includes/templates/header.php';?>
+
+
+
+
+
+<?php require 'includes/templates/footer.php';?>
