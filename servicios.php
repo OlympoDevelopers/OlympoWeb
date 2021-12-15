a8ae111582783d4ffe49988ff3e103a93aefcfe4
anghelr14 <72480694+anghelr14@users.noreply.github.com>
2021-12-15 10:26:03 -0600

    Segunda propuesta boton

--- servicios.php
+++ servicios.php
@@ -7,7 +7,56 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa cupiditate deleniti doloribus eius error eum fugiat, fugit incidunt, laborum necessitatibus officia quae quaerat quisquam quo, repellat saepe suscipit vel!</p>
 
         <div class="btn">
-            <span>Contratar</span>
+<!--            <div class="shop-now">Contratar</div>-->
+            <a href="#" class="shop-now">Contratar</a>
+            <div class="snowflake-grid to-left">
+                <div class="snowflake-item border-bottom border-right">
+                    <div class="sub-items border-right border-bottom pull-down">
+                        <div class="m-w-15 m-h-15 border-right border-bottom m-3"></div>
+                    </div>
+                </div>
+                <div class="snowflake-item border-bottom border-left">
+                    <div
+                            class="sub-items border-right border-bottom r-90 pull-down-right"
+                    >
+                        <div class="m-w-15 m-h-15 border-right border-bottom m-3"></div>
+                    </div>
+                </div>
+                <div class="snowflake-item border-top border-right">
+                    <div class="sub-items border-right border-bottom r-270 pull-right">
+                        <div class="m-w-15 m-h-15 border-right border-bottom m-3"></div>
+                    </div>
+                </div>
+                <div class="snowflake-item border-top border-left">
+                    <div class="sub-items border-right border-bottom r-180 pull-left">
+                        <div class="m-w-15 m-h-15 border-right border-bottom m-3"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="snowflake-grid to-right">
+                <div class="snowflake-item border-bottom border-right">
+                    <div class="sub-items border-right border-bottom pull-down">
+                        <div class="m-w-15 m-h-15 border-right border-bottom m-3"></div>
+                    </div>
+                </div>
+                <div class="snowflake-item border-bottom border-left">
+                    <div
+                            class="sub-items border-right border-bottom r-90 pull-down-right"
+                    >
+                        <div class="m-w-15 m-h-15 border-right border-bottom m-3"></div>
+                    </div>
+                </div>
+                <div class="snowflake-item border-top border-right">
+                    <div class="sub-items border-right border-bottom r-270 pull-right">
+                        <div class="m-w-15 m-h-15 border-right border-bottom m-3"></div>
+                    </div>
+                </div>
+                <div class="snowflake-item border-top border-left">
+                    <div class="sub-items border-right border-bottom r-180 pull-left">
+                        <div class="m-w-15 m-h-15 border-right border-bottom m-3"></div>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
