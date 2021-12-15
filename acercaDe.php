b6cfa4963c5123025e31e1b716933c4b59318b17
anghelr14 <72480694+anghelr14@users.noreply.github.com>
2021-12-14 23:28:00 -0600

    Acerca de version aceptada

--- acercaDe.php
+++ acercaDe.php
@@ -6,6 +6,7 @@
 
         <div class="acercade">
             <h2>Acerca de...</h2>
+            <img src="/build/img/team.webp" alt="team de trabajo">
         </div>
         <div class="resumen">
             <p>Olympo Project comenzó a planificarse en septiembre del año 2020, surgió a base de el entusiasmo y orgullo de cuatro estudiantes llamados Gonzales Moralez Anghel Raul, Curiel Aportela Pedro Iván, Baltazar Reyes Oscar Yael y Rodriguez Hernández Marco Antonio, el objetivo que teniamos al principio era la creacion de videojuegos sin embargo en el transcurso del desarrollo de nuestro primer proyecto nos dimos cuenta que no contabamos con los recursos para concluirlo. Asi es como optamos por cambiar el enfoque de la empresa, brindando un servicio para que las personas que quieren iniciar un proyecto enfocado al desarrollo digital puedan realizarlo, Logi-K es un servicio que ofrece apoyo en programación, diseño en voxeles, desarrollo web, animación y edición de material audiovisual.
