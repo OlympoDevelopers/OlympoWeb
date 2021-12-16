e1144eba02c3788a2a3c4e012e2ce9c12ea82aeb
anghelr14 <72480694+anghelr14@users.noreply.github.com>
2021-12-16 10:49:03 -0600

    Deployment imagenes

--- conocenos.php
+++ conocenos.php
@@ -35,9 +35,9 @@
 
             <div class="tarjeta-equipo">
 
-                <img src="/build/img/kaboompics_Summer%20Campfire.jpg" alt="Perfil Ivan Fondo" class="card-imgbg">
+                <img src="/build/img/kaboompics_Summer%20Campfire.webp" alt="Perfil Ivan Fondo" class="card-imgbg">
 
-                <img src="/build/img/ivan.avif" alt="Perfil Ivan" class="card-pbg">
+                <img src="/build/img/ivan.webp" alt="Perfil Ivan" class="card-pbg">
                 <h1>Curiel Aportela</h1>
                 <p class="rol-a-cargo">Diseñador Voxel</p>
                 <p class="acerca-rol">
@@ -63,7 +63,7 @@
 
                 <img src="/build/img/webDesign.webp" alt="Perfil Anghel Fondo" class="card-imgbg">
 
-                <img src="/build/img/marco.avif" alt="Perfil Marco" class="card-pbg">
+                <img src="/build/img/marco.webp" alt="Perfil Marco" class="card-pbg">
                 <h1>Marco Antonio</h1>
                 <p class="rol-a-cargo">Animador y Editor</p>
                 <p class="acerca-rol">
@@ -87,9 +87,9 @@
 
 <div class="tarjeta-equipo">
 
-                <img src="/build/img/webDesign.webp" alt="Perfil Anghel Fondo" class="card-imgbg">
+                <img src="/build/img/pexels-stein-egil-liland-1933318.webp" alt="Perfil Anghel Fondo" class="card-imgbg">
 
-                <img src="/build/img/oscar.avif" alt="Perfil Oscar" class="card-pbg">
+                <img src="/build/img/oscar.webp" alt="Perfil Oscar" class="card-pbg">
                 <h1>Oscar Baltazar</h1>
                 <p class="rol-a-cargo">Desarrollador de Software</p>
                 <p class="acerca-rol">
--- servicios.php
+++ servicios.php
@@ -242,7 +242,7 @@
     </div>
 
     <div class="card">
-        <img src="/build/img/pexels-markus-spiske-360591.jpg" alt="Desarrollo de software">
+        <img src="/build/img/pexels-markus-spiske-360591.avif" alt="Desarrollo de software">
         <h4>Desarrollo de Software</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda culpa cupiditate deleniti doloribus eius error eum fugiat, fugit incidunt, laborum necessitatibus officia quae quaerat quisquam quo, repellat saepe suscipit vel!</p>
 
