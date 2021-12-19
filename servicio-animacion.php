d1f0a85ec74bc0997e9842eb3edab94127005be3
marco <1900400241-4@netmexico.com>
2021-12-19 16:48:21 -0600

    Agregue tarjetas de Animación y css, agregue las hojas no funcionales sobre la edicion de video

--- servicio-animacion.php
+++ servicio-animacion.php
@@ -3,37 +3,34 @@
 <div class="grid">
     <div class="grid__item">
         <div class="card">
-            <img src="build/img/animacion1.webp" alt="Animacion 1" class="card__img">
+            <img src="build/img/Animaciones1.webp" alt="Animaciones 1" class="card__img">
             <div class="card__content">
-                <h1 class="card__header">A starry night</h1>
-                <p class="card__text">Look up at the night sky, and find yourself <strong>immersed</strong> in the
-                    amazing mountain range of Aspen. </p>
-                <button class="card__btn">Explore <span>&rarr;</span></button>
+                <h1 class="card__header">Animación de modelos esqueleticos</h1>
+                <p class="card__text">Refiere a la animacion de un modelo 3D que contenga un esqueleto como un humano o animal. </p>
+                <button class="card__btn">Cotizar <span>&rarr;</span></button>
             </div>
         </div>
     </div>
     <div class="grid__item">
         <div class="card">
             <img class="card__img"
-                 src="build/img/animacion2.webp"
-                               alt="Animacion 2">
+                 src="build/img/Animaciones2.webp"
+                               alt="Animaciones 2">
             <div class="card__content">
-                <h1 class="card__header">Misty mornings</h1>
-                <p class="card__text">Capture the stunning <strong>essence</strong> of the early morning sunrise in the
-                    Californian wilderness.</p>
-                <button class="card__btn">Explore <span>&rarr;</span></button>
+                <h1 class="card__header">Animación de objetos</h1>
+                <p class="card__text">Refiere a la animacion de objetos que no tengan esqueletos como un cubo o forma geometrica, objetos de la vida común.</p>
+                <button class="card__btn">Cotizar <span>&rarr;</span></button>
             </div>
         </div>
     </div>
     <div class="grid__item">
         <div class="card"><img class="card__img"
-                               src="https://images.unsplash.com/photo-1506318164473-2dfd3ede3623?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=3300&amp;q=80"
-                               alt="Canyons">
+                               src="build/img/Animaciones3.webp"
+                               alt="Animaciones 3">
             <div class="card__content">
-                <h1 class="card__header">Utah sunsets</h1>
-                <p class="card__text">Sunsets over the <strong>stunning</strong> Utah Canyonlands, is truly something
-                    much more than incredible.</p>
-                <button class="card__btn">Explore <span>&rarr;</span></button>
+                <h1 class="card__header">Animación por tiempo</h1>
+                <p class="card__text">Refiere a que puede contener tanto objetos con esqueleto y objetos sin esqueleto, el presupuestado sera dependeiendo del tiempo requerido.</p>
+                <button class="card__btn">Cotizar <span>&rarr;</span></button>
             </div>
         </div>
     </div>
