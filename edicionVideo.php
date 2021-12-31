65e6753d0c16cf22a7a5d2cd19bdedce13f31400
marco <1900400241-4@netmexico.com>
2021-12-31 16:00:32 -0600

    Parte 2 de proceso de incluir tarjetas de edicion de video

--- edicionVideo.php
+++ edicionVideo.php
@@ -1,8 +1,8 @@
 <?php require 'includes/templates/header.php'; ?>
 <ul class="cartasVideo">
     <li>
-        <a href="" class="cartas">
-            <img src="build/img/" class="cardImage" alt="" />
+        <a href="" class="cartas1">
+            <img src="build/img/EVideos1.webp" class="cardImage" alt="" />
             <div class="cardOverlay">
                 <div class="cardHeader">
                     <svg class="cardArc" xmlns="http://www.w3.org/2000/svg"></svg>
@@ -17,8 +17,8 @@
         </a>
     </li>
     <li>
-        <a href="" class="cartas">
-            <img src="https://i.imgur.com/2DhmtJ4.jpg" class="card__image" alt="" />
+        <a href="" class="cartas2">
+            <img src="build/img/EVideos2.webp" class="cardImage" alt="" />
             <div class="cardOverlay">
                 <div class="cardHeader">
                     <svg class="cardArc" xmlns="http://www.w3.org/2000/svg"></svg>
@@ -32,38 +32,6 @@
             </div>
         </a>
     </li>
-    <li>
-        <a href="" class="cartas">
-            <img src="https://i.imgur.com/oYiTqum.jpg" class="cardImage" alt="" />
-            <div class="cardOverlay">
-                <div class="cardHeader">
-                    <svg class="cardArc" xmlns="http://www.w3.org/2000/svg">></svg>
-                    <img class="cardThumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
-                    <div class="cardHeaderText">
-                        <h3 class="cardTitulo">Jessica Parker</h3>
-                        <span class="cardTagline">Lorem ipsum dolor sit amet consectetur</span>
-                        <span class="cardStatus">1 hour ago</span>
-                    </div>
-                </div>
-                <p class="cardDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-            </div>
-        </a>
-    </li>
-    <li>
-        <a href="" class="card">
-            <img src="https://i.imgur.com/2DhmtJ4.jpg" class="cardImage" alt="" />
-            <div class="cardOverlay">
-                <div class="cardHeader">
-                    <svg class="cardArc" xmlns="http://www.w3.org/2000/svg"></svg>
-                    <img class="cardThumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
-                    <div class="cardHeaderText">
-                        <h3 class="cardTitulo">kim Cattrall</h3>
-                        <span class="cardStatus">3 hours ago</span>
-                    </div>
-                </div>
-                <p class="cardDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-            </div>
-        </a>
-    </li>
+
 </ul>
 <?php require 'includes/templates/footer.php'; ?>
