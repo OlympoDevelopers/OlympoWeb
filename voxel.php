b90b4840dc6a210dddf6d57eac183b790d20c18d
IvanCuri <Ivan.Ap2020@outlook.com>
2022-01-04 22:58:07 -0600

    pagina voxels

---
+++ voxel.php
@@ -0,0 +1,42 @@
+<?php require 'includes/templates/header.php'; ?>
+
+<div class="cuadricula">
+    <div class="grid__item">
+        <div class="card">
+            <img src="build/img/animacion1.webp" alt="Animacion 1" class="card__img">
+            <div class="card__content">
+                <h1 class="card__header">Figura Pequeña</h1>
+                <p class="card__text">Look up at the night sky, and find yourself <strong>immersed</strong> in the
+                    amazing mountain range of Aspen. </p>
+               
+            </div>
+        </div>
+    </div>
+    <div class="grid__item">
+        <div class="card">
+            <img class="card__img"
+                 src="build/img/animacion2.webp"
+                               alt="Animacion 2">
+            <div class="card__content">
+                <h1 class="card__header">Figura mediana</h1>
+                <p class="card__text">Capture the stunning <strong>essence</strong> of the early morning sunrise in the
+                    Californian wilderness.</p>
+               
+            </div>
+        </div>
+    </div>
+    <div class="grid__item">
+        <div class="card"><img class="card__img"
+                               src="https://images.unsplash.com/photo-1506318164473-2dfd3ede3623?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=3300&amp;q=80"
+                               alt="Canyons">
+            <div class="card__content">
+                <h1 class="card__header">Figura + Escenario</h1>
+                <p class="card__text">Sunsets over the <strong>stunning</strong> Utah Canyonlands, is truly something
+                    much more than incredible.</p>
+               
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require 'includes/templates/footer.php'; ?>
